8324488485ddc6d52914ace8adbb035ddd0f20b3
Yueqi Wang <yueqi.wang.pku@gmail.com>
2018-11-11 06:30:20 -0500

    fix path

--- src/run_gqn_face3d.sh
+++ src/run_gqn_face3d.sh
@@ -1,3 +1,3 @@
-python run_gqn_face3d.py --data_dir /media/yueqi/Data1/data_ml/gqn_dataset/face3d_20181111-032039/ \
+python run_gqn_face3d.py --data_dir /media/yueqi/Data1/data_ml/gqn_dataset/face3d_20181111-032039/train/ \
     --gradient_steps 10000 --batch_size 20 --save_every 100 --print_every 10 --output_dir ../output
     
