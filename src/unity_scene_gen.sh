32ed9f62be8a3a1fd578bd3b14b326573a15fdf8
Yueqi Wang <yueqi.wang.pku@gmail.com>
2018-12-02 13:37:11 -0500

    unity data generator

---
+++ src/unity_scene_gen.sh
@@ -0,0 +1,11 @@
+python unity_scene_generator.py --env /home/yueqi/Dropbox/lib/ml-agents/build/PyramidsCamera_internal_linux64 \
+--output_dir /home/yueqi/Dropbox/data_large/gqn_dataset/PyramidsCamera1 --n 10000 --nstart 0
+
+python unity_scene_generator.py --env /home/yueqi/Dropbox/lib/ml-agents/build/PushBlockCamera_internal_linux64 \
+--output_dir /home/yueqi/Dropbox/data_large/gqn_dataset/PushBlockCamera1 --n 10000 --nstart 0
+
+python unity_scene_generator.py --env /home/yueqi/Dropbox/lib/ml-agents/build/HallwayCamera_internal_linux64 \
+--output_dir /home/yueqi/Dropbox/data_large/gqn_dataset/HallwayCamera1 --n 10000 --nstart 0
+
+
+
---
+++ src/unity_scene_generator.py
@@ -0,0 +1,48 @@
+from mlagents.envs import UnityEnvironment
+import numpy as np 
+import pickle, os
+from collections import defaultdict
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Generate Unity scenes (internal brain).')
+parser.add_argument('--env', type=str, help='path to game build')
+parser.add_argument('--output_dir', type=str, help='location of output data')
+parser.add_argument('--n', type=int, default=1000, help='number of episodes')
+parser.add_argument('--nstart', type=int, default=0, help='number to start from')
+parser.add_argument('--print_every', type=int, default=100, help='print progress')
+args = parser.parse_args()
+
+env = UnityEnvironment(file_name=args.env)
+# Set the default brain to work with
+default_brain = env.brain_names[0]
+
+train_mode = True 
+
+env_info = env.reset(train_mode=train_mode)[default_brain]
+
+if not os.path.exists(args.output_dir):
+    os.makedirs(args.output_dir)
+
+count = args.nstart
+batch_size = len(env_info.agents)
+to_store = [defaultdict(list) for _ in range(batch_size)]
+
+last_count = -1
+while count < args.n:
+    if (count) % args.print_every == 0 and count != last_count:
+        print(count)
+        last_count = count
+    env_info = env.step()[default_brain]
+    for i in range(batch_size):
+        to_store[i]['previous_action'].append(env_info.previous_vector_actions[i])
+        to_store[i]['reward'].append(env_info.rewards[i])
+        to_store[i]['vector_observation'].append(env_info.vector_observations[i])
+        to_store[i]['visual_observation'].append(env_info.visual_observations[0][i])
+        if env_info.local_done[i]:
+            count += 1
+            with open(os.path.join(args.output_dir, str(count)+'.p'), "wb") as f:
+                pickle.dump(to_store[i], f)
+            to_store[i] = defaultdict(list)
+
+print("Done!")
