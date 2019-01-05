a091cc803fbc9cbbfb1e7df08c9bb5124fad9b19
Yueqi Wang <yueqi.wang.pku@gmail.com>
2019-01-05 02:56:06 -0500

    update example

--- src/example_running_scripts.sh
+++ src/example_running_scripts.sh
@@ -1,57 +1,16 @@
-python run_gqn_face3d.py \
---data_dir /home/yueqi/Data1/data_ml/gqn_dataset/face3d_20181111-032039/train/ \
---gradient_steps 10000 \
---batch_size 20 \
---save_every 500 \
---print_every 500 \
---output_dir ../output
 
+## 3D Unity games
 
 python run_gqn_unity.py \
 --data_dir /home/yueqi/EVO/gqn_dataset/HallwayCamera2/train \
 --model_name HallwayCamera \
 --output_dir ../output \
---n_timesteps 10 \
+--n_timesteps 8 \
 --gradient_steps 20000 \
---batch_size 20 \
---save_every 500 \
---print_every 500
-
-
-    
-python run_gqn_unity.py \
---data_dir /home/yueqi/EVO/gqn_dataset/PyramidsCamera2/train \
---model_name PyramidsCamera \
---output_dir ../output \
---n_timesteps 10 \
---gradient_steps 20000 \
---batch_size 20 \
---save_every 500 \
---print_every 500
-
-
-python run_gqn_unity.py \
---data_dir /home/yueqi/EVO/gqn_dataset/PushBlockCamera2/train \
---model_name PushBlockCamera \
---output_dir ../output \
---n_timesteps 10 \
---gradient_steps 20000 \
---batch_size 20 \
---save_every 500 \
---print_every 500
-
-
-
-    
-python run_gqn_unity.py \
---data_dir ~/gqn_dataset/PyramidsCamera2/train \
---model_name PyramidsCamera \
---output_dir ../output \
---n_timesteps 10 \
---gradient_steps 50000 \
 --batch_size 36 \
---save_every 500 \
---print_every 500
+--save_every 1000 \
+--print_every 50
+
 
 
 python run_gqn_unity.py \
@@ -76,3 +35,15 @@ python run_gqn_unity.py \
 --batch_size 36 \
 --save_every 1000 \
 --print_every 50
+
+
+
+## face3d
+
+python run_gqn_face3d.py \
+--data_dir /home/yueqi/Data1/data_ml/gqn_dataset/face3d_20181111-032039/train/ \
+--gradient_steps 10000 \
+--batch_size 20 \
+--save_every 500 \
+--print_every 500 \
+--output_dir ../output
